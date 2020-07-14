38630a287ea9a33bacef7353a537f37d4bc2ba9b
Cristiano Souza <cristiano.sigm@gmail.com>
2020-07-14 09:55:22 -0300

    Initial code

---
+++ src/test/java/cs/home/health/HealthApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.home.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
